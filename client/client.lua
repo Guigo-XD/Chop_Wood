272e6b0ea82e0c93f3545a0bc3a97931a5a2713b
Guigo-XD <guilherme.coelho04@gmail.com>
2023-08-21 22:18:31 -0300

    fix

--- client/client.lua
+++ client/client.lua
@@ -1,16 +1,12 @@
-
 MenuData = {}
-TriggerEvent("redemrp_menu_base:getData",function(call)
+TriggerEvent("redemrp_menu_base:getData", function(call)
     MenuData = call
 end)
 
 local ChopPrompt
-local ChopPrompts = GetRandomIntInRange(0, 0xffffff)
+local hasAlreadyEnteredMarker = false
 
-local ChopScenarios = {
-	'PROP_HUMAN_WOOD_CHOP',
-	'PROP_HUMAN_WOOD_CHOP_MALE_A', -- verify others
-}
+local ChopScenario = 'PROP_HUMAN_WOOD_CHOP'
 
 
 function SetupChopPrompt()
@@ -24,67 +20,76 @@ function SetupChopPrompt()
         PromptSetVisible(ChopPrompt, false)
         PromptSetHoldMode(ChopPrompt, true)
         PromptRegisterEnd(ChopPrompt)
-
     end)
 end
 
 Citizen.CreateThread(function()
+    for z, x in pairs(Config.Stomps) do
+        while not HasModelLoaded(GetHashKey('p_stumpwoodsplit02x')) do
+            Wait(500)
+            modelrequest(GetHashKey('p_stumpwoodsplit02x'))
+        end
+        local stump = CreateObject(GetHashKey('p_stumpwoodsplit02x'), Config.Stomps[z]["Pos"].x,
+            Config.Stomps[z]["Pos"].y, Config.Stomps[z]["Pos"].z, true, true, false)
+        PlaceObjectOnGroundProperly(stump)
+        SetModelAsNoLongerNeeded(GetHashKey('p_stumpwoodsplit02x'))
+    end
+end)
+
+Citizen.CreateThread(function()
     SetupChopPrompt()
+    local SelectScenario
     while true do
         local t = 500
 
-		local DataStruct = DataView.ArrayBuffer(256 * 4)
-		local scenarios = Citizen.InvokeNative(0x345EC3B7EBDE1CB5, GetEntityCoords(PlayerPedId()), 1.0, DataStruct:Buffer(), 10)	-- GetScenarioPointsInArea
-
-		if scenarios then
+        local DataStruct = DataView.ArrayBuffer(256 * 4)
+        local scenarios = Citizen.InvokeNative(0x345EC3B7EBDE1CB5, GetEntityCoords(PlayerPedId()), 1.5,
+            DataStruct:Buffer(), 10)
+        if scenarios and not hasAlreadyEnteredMarker then
             for i = 1, scenarios do
                 local scenario = DataStruct:GetInt32(8 * i)
-                local scenario_hash = Citizen.InvokeNative(0xA92450B5AE687AAF, scenario)	-- GetScenarioPointType
+                local scenario_hash = Citizen.InvokeNative(0xA92450B5AE687AAF, scenario) -- GetScenarioPointType
 
-                for _, v in pairs(ChopScenarios) do
-                    if GetHashKey(v) == scenario_hash and not active then
-                        local label  = CreateVarString(10, 'LITERAL_STRING', Config.Texts['ObjectChop'])
-                        PromptSetActiveGroupThisFrame(ChopPrompts, label)
-                        
-                        if PromptHasHoldModeCompleted(ChopPrompt) then
-                            Citizen.Wait(500)
-                       -- verifica no server se tem machado, madeira
-                        local scenario = DataStruct:GetInt32(8 * i)
-                       TriggerServerEvent('woodstump:CheckAxe',scenario)
-                        end
+                if GetHashKey(ChopScenario) == scenario_hash and not hasAlreadyEnteredMarker then
+                    hasAlreadyEnteredMarker = true
+                    if not IsPedActiveInScenario(PlayerPedId()) then
+                        PromptSetEnabled(ChopPrompt, true)
+                        PromptSetVisible(ChopPrompt, true)
                     end
-
+                    SelectScenario = scenario
                 end
+            end
+        else
+            if not scenarios then
+                hasAlreadyEnteredMarker = false
+                PromptSetEnabled(ChopPrompt, false)
+                PromptSetVisible(ChopPrompt, false)
             end
         end
 
-		Citizen.Wait(t)
+        if PromptHasHoldModeCompleted(ChopPrompt) then
+            Citizen.Wait(100)
+            -- verifica no server se tem machado, madeira , configuravel
+            TriggerServerEvent('woodstump:CheckAxe', SelectScenario)
+        end
 
+        Citizen.Wait(t)
     end
 end)
 
 RegisterNetEvent('woodstump:Chop')
 AddEventHandler('woodstump:Chop', function(scenario)
-	active = true
+    active = true
 
-	TaskUseScenarioPoint(PlayerPedId(), scenario, '' , -1.0, true, false, 0, false, -1.0, true)
-    
-	local progressbar = exports.progressbar:initiate() -- verificar 
-	progressbar.start(Config.Texts['chopping'], 15000, function ()
+    TaskUseScenarioPoint(PlayerPedId(), scenario, '', -1.0, true, false, 0, false, -1.0, true)
+    Citizen.Wait(5000)
+    while IsPedActiveInScenario(PlayerPedId()) do
+        Citizen.Wait(200)
+    end
 
-		ClearPedTasks(PlayerPedId(), true)
-
-		TriggerServerEvent('woodstump:AddFirewood')
-
-        Wait(1000)
-
-        active = false
-
-	end, 'innercircle', Config.ProgressbarColor)
-
+    print('Call - AddFirewood')
+    TriggerServerEvent('woodstump:AddFirewood')
 end)
-
-
 
 
 
@@ -105,12 +110,12 @@ DataView = {
         Int64 = { code = 'i8', size = 8 },
         Uint64 = { code = 'I8', size = 8 },
 
-        LuaInt = { code = 'j', size = 8 }, 
-        UluaInt = { code = 'J', size = 8 }, 
-        LuaNum = { code = 'n', size = 8}, 
+        LuaInt = { code = 'j', size = 8 },
+        UluaInt = { code = 'J', size = 8 },
+        LuaNum = { code = 'n', size = 8 },
         Float32 = { code = 'f', size = 4 },
-        Float64 = { code = 'd', size = 8 }, 
-        String = { code = 'z', size = -1, }, 
+        Float64 = { code = 'd', size = 8 },
+        String = { code = 'z', size = -1, },
     },
 
     FixedTypes = {
@@ -128,24 +133,30 @@ function DataView.ArrayBuffer(length)
         offset = 1, length = length, blob = _strblob(length)
     }, DataView)
 end
+
 function DataView.Wrap(blob)
     return setmetatable({
         offset = 1, blob = blob, length = blob:len(),
     }, DataView)
 end
+
 function DataView:Buffer() return self.blob end
+
 function DataView:ByteLength() return self.length end
+
 function DataView:ByteOffset() return self.offset end
+
 function DataView:SubView(offset)
     return setmetatable({
         offset = offset, blob = self.blob, length = self.length,
     }, DataView)
 end
-for label,datatype in pairs(DataView.Types) do
+
+for label, datatype in pairs(DataView.Types) do
     DataView['Get' .. label] = function(self, offset, endian)
         local o = self.offset + offset
         if _ib(o, self.length, datatype) then
-            local v,_ = string.unpack(_ef(endian) .. datatype.code, self.blob, o)
+            local v, _ = string.unpack(_ef(endian) .. datatype.code, self.blob, o)
             return v
         end
         return nil
@@ -164,12 +175,12 @@ for label,datatype in pairs(DataView.Types) do
         return nil
     end
 end
-for label,datatype in pairs(DataView.FixedTypes) do
+for label, datatype in pairs(DataView.FixedTypes) do
     DataView['GetFixed' .. label] = function(self, offset, typelen, endian)
         local o = self.offset + offset
         if o + (typelen - 1) <= self.length then
             local code = _ef(endian) .. 'c' .. tostring(typelen)
-            local v,_ = string.unpack(code, self.blob, o)
+            local v, _ = string.unpack(code, self.blob, o)
             return v
         end
         return nil
@@ -185,8 +196,8 @@ for label,datatype in pairs(DataView.FixedTypes) do
 end
 
 SetFixed = function(self, offset, value, code)
-    local fmt = { }
-    local values = { }
+    local fmt = {}
+    local values = {}
     if self.offset < offset then
         local size = offset - self.offset
         fmt[#fmt + 1] = 'c' .. tostring(size)
@@ -207,20 +218,20 @@ SetFixed = function(self, offset, value, code)
     return self
 end
 
-DataStream = { }
+DataStream = {}
 DataStream.__index = DataStream
 
 function DataStream.New(view)
     return setmetatable({ view = view, offset = 0, }, DataStream)
 end
 
-for label,datatype in pairs(DataView.Types) do
+for label, datatype in pairs(DataView.Types) do
     DataStream[label] = function(self, endian, align)
         local o = self.offset + self.view.offset
         if not _ib(o, self.view.length, datatype) then
             return nil
         end
-        local v,no = string.unpack(_ef(endian) .. datatype.code, self.view:Buffer(), o)
+        local v, no = string.unpack(_ef(endian) .. datatype.code, self.view:Buffer(), o)
         if align then
             self.offset = self.offset + math.max(no - o, align)
         else
